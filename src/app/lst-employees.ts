3ba18acd85bee2eded29ec2dcc5267b2b888f7e7
Никонов Михаил Андреевич <nikonov.m@luxoptica.com.ua>
2018-10-31 15:57:03 +0200

    2ok

---
+++ src/app/lst-employees.ts
@@ -0,0 +1,16 @@
+import { Employee } from './employee';
+
+
+export const EMPLOYEES: Employee[] = [
+
+    {
+      id: 1, tel_inner: '8053',
+      job: 'Веб разработчик',
+      name: 'Никонов Михаил Андреевич',
+      tel_outer: '065-548-78-98',
+      email: 'nikonov.m@luxoptica.com.ua',
+      dep: 'IT', color: 'White', depar: ''
+    }
+
+  ];
+
